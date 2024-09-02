c9278c46956be834b55253180ab2c295a1c027b2
Bryan Bliewert <bryan.bliewert@nventis.eu>
2024-09-02 16:17:54 +0200

    Added fix for imports of phc on Windows

--- phc/tasks/__init__.py
+++ phc/tasks/__init__.py
@@ -1,3 +1,8 @@
-from .BaseTask import BaseTask
-from .ShellTask import ShellTask
-from .ForcibleTask import ForcibleTask
+from os import name
+
+if name != 'nt':
+    from .BaseTask import BaseTask
+    from .ShellTask import ShellTask
+    from .ForcibleTask import ForcibleTask
+    
+del name
