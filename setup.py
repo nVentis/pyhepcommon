2857dff1643258e45fed5dc34730c13638e24620
Bryan Bliewert <bryan.bliewert@nventis.eu>
2024-10-14 20:29:31 +0200

    Fixed name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyhepcommon',
+    name='phc',
     version='0.0.1',
     url='https://github.com/nVentis/pyhepcommon',
     author='Bryan Bliewert',
